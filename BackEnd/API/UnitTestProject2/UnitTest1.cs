3478fcd6251db01cf6f7334f349b14f8b414abf9
matacker <42584708+matacker@users.noreply.github.com>
2020-04-05 12:34:19 -0600

    Revert "Adding Edits to the test"
    This reverts commit becc9a7fcc21984ae7779db41fe6ddbdbfe4aeeb.

--- BackEnd/API/UnitTestProject2/UnitTest1.cs
+++ BackEnd/API/UnitTestProject2/UnitTest1.cs
@@ -3,7 +3,6 @@ using HidGlobal.OK.Readers.AViatoR.Components;
 using HidGlobal.OK.Readers.Components;
 using HidGlobal.OK.SampleCodes.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace UnitTestProject2
 {
@@ -15,39 +14,26 @@ namespace UnitTestProject2
         {
             var reader = new SmartCardReader(readerName);
 
-            //TODO: We need a to arange the test so it tests the key and authentiactes.
+            ReaderHelper.ConnectToReaderWithCard(reader);
 
-            var test = new SmartCardReader(readerName);
-           
-
-            ReaderHelper.ConnectToReader(reader);
-
-            ReaderHelper.ConnectToReader(test);//connecting test to the reader
-
-            ReaderHelper.GeneralAuthenticateMifare(test, "Authenticate with key ", 0x01, GeneralAuthenticateCommand.MifareKeyType.MifareKeyA, 0x02); //sending test to be authenticated
-            ReaderHelper.ReadBinaryMifareCommand(test, "Read Binary block nr ", 0x01, 0x00); 
-
-            ReaderHelper.GeneralAuthenticateMifare(reader, "Authenticate with key from slot nr ", 0x01,
+            ReaderHelper.GeneralAuthenticateMifare(reader, "Authenticate with key from slot nr ", 0x04,
                 GeneralAuthenticateCommand.MifareKeyType.MifareKeyA, 0x02);
-            
-            ReaderHelper.ReadBinaryMifareCommand(reader, "Read Binary block nr ", 0x01, 0x00);
+            ReaderHelper.ReadBinaryMifareCommand(reader, "Read Binary block nr ", 0x04, 0x00);
 
-            Assert.AreSame(reader, test); //Assert test to compare if reader and test are the same
+            ReaderHelper.GeneralAuthenticateMifare(reader, "Authenticate with key from slot nr ", 0x05,
+                GeneralAuthenticateCommand.MifareKeyType.MifareKeyA, 0x02);
+            ReaderHelper.ReadBinaryMifareCommand(reader, "Read Binary block nr ", 0x05, 0x00);
 
-            //ReaderHelper.GeneralAuthenticateMifare(reader, "Authenticate with key from slot nr ", 0x05,
-               // GeneralAuthenticateCommand.MifareKeyType.MifareKeyA, 0x02);
-           // ReaderHelper.ReadBinaryMifareCommand(reader, "Read Binary block nr ", 0x05, 0x00);
+            ReaderHelper.GeneralAuthenticateMifare(reader, "Authenticate with key from slot nr ", 0x06,
+                GeneralAuthenticateCommand.MifareKeyType.MifareKeyA, 0x02);
+            ReaderHelper.ReadBinaryMifareCommand(reader, "Read Binary block nr ", 0x06, 0x00);
 
-           // ReaderHelper.GeneralAuthenticateMifare(reader, "Authenticate with key from slot nr ", 0x06,
-            //    GeneralAuthenticateCommand.MifareKeyType.MifareKeyA, 0x02);
-          //  ReaderHelper.ReadBinaryMifareCommand(reader, "Read Binary block nr ", 0x06, 0x00);
-
-          //  ReaderHelper.GeneralAuthenticateMifare(reader, "Authenticate with key from slot nr ", 0x07,
-         //       GeneralAuthenticateCommand.MifareKeyType.MifareKeyA, 0x02);
-           // ReaderHelper.ReadBinaryMifareCommand(reader, "Read Binary block nr ", 0x07, 0x00);
+            ReaderHelper.GeneralAuthenticateMifare(reader, "Authenticate with key from slot nr ", 0x07,
+                GeneralAuthenticateCommand.MifareKeyType.MifareKeyA, 0x02);
+            ReaderHelper.ReadBinaryMifareCommand(reader, "Read Binary block nr ", 0x07, 0x00);
 
             reader.Disconnect(CardDisposition.Unpower);
-            test.Disconnect(CardDisposition.Unpower);
+
         }
     }
 }
