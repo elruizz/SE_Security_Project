be9ee78b63829b5691760272671de0d6b4753024
Ethan Ruiz <45ruiz@gmail.com>
2020-05-10 00:09:00 -0600

    Working Exe App

---
+++ OmniApp/OmikeyMifareK-win32-x64/resources/app/MifareWriteBlockData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace HidGlobal.OK.Readers
+{
+    class MifareWriteBlockData
+    {
+        
+        public async Task<object>
+           Invoke(dynamic input)
+        {
+            var _write = new MifareAPI.UpdateMifareClassic1k();
+            var InitMifareBackEnd = new MifareAPI.InitReader();
+            string _input = (string)input._input;
+            byte _blockinput = Byte.Parse(input._blockinput, NumberStyles.HexNumber);
+            ISmartCardReader WebReader = InitMifareBackEnd.RunInitReader();
+            WebReader = InitMifareBackEnd.RunInitReader();
+            return _write.RunWriteMifare(_input, _blockinput);
+        }
+    }
+}
